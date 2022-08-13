0f36fe6d17d95746ec4a515c473dd31783891d87
Qin Zhen <22120899@bjtu.edu.cn>
2022-08-13 12:27:28 +0800

    add Chapter01 homework

---
+++ Chapter01/homework01.java
@@ -0,0 +1,8 @@
+// print hello,world
+
+public class homework01 {
+	public static void main(String[] args) {
+		System.out.println("hello,world");
+	}
+
+}
---
+++ Chapter01/homework02.java
@@ -0,0 +1,6 @@
+public class homework02 {
+
+	public static void main(String[] args) {
+		System.out.println("姓名\t性别\t籍贯\t住址\n秦朕\t男\t安徽\t安徽");
+	}
+}
