0fdcd0035a7b5bef2a77d718872b951633bc6fbc
Qin Zhen <22120899@bjtu.edu.cn>
2022-08-13 12:28:48 +0800

    add Chapter02 homework

---
+++ Chapter02/homework03.java
@@ -0,0 +1,17 @@
+public class homework03 {
+
+	public static void main(String[] args) {
+
+		String book1 = "书名1";
+		String book2 = "书名2";
+		System.out.println(book1 + book2);	// 书名1书名2
+
+		char c1 = '男';
+		char c2 = '女';
+		System.out.println(c1 + c2);	// GBK编码，输出两个汉字对应数值的加和，结果为数值
+
+		double p1 = 50.5;
+		double p2 = 60.8;
+		System.out.println(p1 + p2);	// 111.3
+	}
+}
---
+++ Chapter02/homework04.java
@@ -0,0 +1,14 @@
+public class homework04 {
+
+	public static void main(String[] args) {
+
+		String name = "胡图图";
+		char gender = '男';
+		short age = 3;
+		double score = 100;
+		String hobby = "吃";
+
+		System.out.println("姓名\t性别\t年龄\t成绩\t爱好\n" + name + '\t' + gender + '\t' + age + '\t' +score + '\t' + hobby);
+
+	}
+}
